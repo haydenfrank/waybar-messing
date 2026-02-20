d86ec09aeed616b8b18021dca2ef1c77485f1943
Hayden Frank <haydog2006@gmail.com>
2026-02-20 11:13:43 -0500

    clock tooltip

--- scripts/clock.sh
+++ scripts/clock.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-date +'%-I:%M %p'
+# Outputs JSON for Waybar custom module with `text` (time) and `tooltip` (date)
+TIME=$(date +'%-I:%M %p')
+DATE=$(date +'%A, %B %-d, %Y')
+printf '{"text":"%s","tooltip":"%s"}\n' "$TIME" "$DATE"
